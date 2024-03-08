96c5c61e608efbc06d9d53b2ffd0e6ecd9dc0b4d
Samarthsinghal28 <120447590+Samarthsinghal28@users.noreply.github.com>
2024-03-09 00:33:18 +0530

    added utility functions

--- functions.h
+++ functions.h
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<string, string> op_map;
-int pc = 0;
+int programCounter = 0;
+int locationAddress = 0;
+string mem[4000];
+int tpc;
+map<string, char> tab = {
+    {"add", 'R'},{"and", 'R'},{"or", 'R'},{"sll", 'R'},{"slt", 'R'},{"sra", 'R'},{"srl", 'R'},{"sub", 'R'},{"xor", 'R'},{"mul", 'R'},{"div", 'R'},
+    {"rem", 'R'},{"addi", 'I'},{"andi", 'I'},{"ori", 'I'},{"lb", 'I'},{"lh", 'I'},{"lw", 'I'},{"jalr", 'I'},{"sb", 'S'},{"sw", 'S'},{"sh", 'S'},
+    {"beq", 'B'},{"bne", 'B'},{"bge", 'B'},{"blt", 'B'},{"auipc", 'U'},{"lui", 'U'},{"jal", 'J'}
+};
+map<string, int> label;
+int ship=0,offset_of_first_load_inst;
+map <string,string> map_op;
+
+
 
 int binaryToDec(string binary);
 int decStringToInt(string decString);
