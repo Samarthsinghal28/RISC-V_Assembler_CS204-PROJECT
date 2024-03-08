b5c20da221a2839dadd84d98192a7b14f585c5ab
Samarthsinghal28 <120447590+Samarthsinghal28@users.noreply.github.com>
2024-03-08 15:46:56 +0530

    header file created

---
+++ functions.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int binaryToDec(string binary);
+int decStringToInt(string decString);
+void loadHexaWord(string s);
+void loadHexHalfWord(string s);
+string convertToLength5(string s);
+string convertToLength20(string s);
+string convertToLength12(string s);
+string decToBinary(int n);
+int binToDec(string binary);
+string binToHexa(string bin);
+string hexaToBinary(string hexdec);
+string rFormatCommands(string line);
+string uFormatCommands(string line);
+string iFormatCommands(string line);
+string sFormatCommands(string line);
+string ujFormatCommands(string line);
+string sbFormatCommands(string line);
+string assemblyToMachine(string line);
